c89e39b674262a092a264bd3462a17358b76f8f8
Werner Van Geit <werner.vangeit@gmail.com>
2025-05-21 15:23:03 +0200

    Add output numbers

--- helper_scripts/add_metadata_inputs.py
+++ helper_scripts/add_metadata_inputs.py
@@ -22,6 +22,20 @@ def main():
             'type': "number"
         }
         template_content["inputs"][input_string] = this_input
+    
+    output_template = template_content["outputs"]["number_output_1"]
+    for output_i in range(1, 21):
+        output_string = f"number_output_{output_i}"
+        if output_string in template_content["outputs"]:
+            del template_content["outputs"][output_string]
+        this_output = output_template.copy()
+        this_output["label"] = output_string
+        this_output["description"] = f"number output {output_i}"
+        this_output["type"] = "ref_contentSchema"
+        this_output["contentSchema"] = {'title': f"number output {output_i}",
+            'type': "number"
+        }
+        template_content["outputs"][output_string] = this_output
 
     new_metadata_path.write_text(
         yaml.dump(json.loads(json.dumps(template_content)), default_flow_style=False, sort_keys=False)
